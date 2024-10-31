637d497471e423403a4a6f8afadf063397e662df
avery <>
2024-10-31 20:25:29 +0100

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from demo.app import hello_world as hello
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), 'Hello Wor1ld!')
+        self.assertEqual(hello(), 'Hello World!')
 
 
 if __name__ == '__main__':
